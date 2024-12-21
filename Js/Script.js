b981e313fced2e4850d1a9901633cc482961fe7c
ChetanMorey44 <146196075+ChetanMorey44@users.noreply.github.com>
2024-12-21 23:37:49 +0530

    first commit

---
+++ Js/Script.js
@@ -0,0 +1,12 @@
+function fun(){
+    document.getElementById('one').style.color='white';
+    document.getElementById('one').style.backgroundColor='black'
+}
+
+function ColorChange(color){
+   document.getElementById('one').style.color=color;
+}
+
+function fun2(){
+   document.getElementById('two').style.display='block';
+}
